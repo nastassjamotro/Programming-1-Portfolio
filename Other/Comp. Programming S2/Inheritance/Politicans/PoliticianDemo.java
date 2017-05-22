37ac77804d8eda43641a32f3a2d2a28afa635a62
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-22 17:34:19 -0600

    Create PoliticianDemo.java

---
+++ Other/Comp. Programming S2/Inheritance/Politicans/PoliticianDemo.java
@@ -0,0 +1,14 @@
+// Nastassja Motro
+
+package politician;
+
+public class PoliticianDemo {
+  public static void main(String[] args) {
+    // Politician generic = new Politician();
+    
+    Trump Donald = new Trump();
+    Clinton Hillary = new clinton();
+    Donald.saySlogan();
+    Hillary.saySlogan();
+  }
+}
