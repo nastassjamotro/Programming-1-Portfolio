a750e284bab24a1e916c56757dde8a701b73745b
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-22 17:38:23 -0600

    Create ShapeDemo.java

---
+++ Other/Comp. Programming S2/Inheritance/Shapes/ShapeDemo.java
@@ -0,0 +1,21 @@
+// Nastassja Motro
+
+package shapes;
+
+public class ShapeDemo {
+  public static void main(String[] args) {
+    // Shape s = new Shape();
+    
+    Circle c = new Circle(10);
+    System.out.println("Circle(10): " + c);
+    
+    Triangle t = new Triangle(6, 7, 12);
+    System.out.println("Triangle(6, 7, 12): " + t);
+    
+    Rectangle r = new Rectangle(5, 9);
+    System.out.println("Rectangle(5, 9): " r);
+    
+    Square s = new Square(3);
+    System.out.println(3): " + s;
+  }
+}
