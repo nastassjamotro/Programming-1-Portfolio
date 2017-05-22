2ba0f66cf2f4426dd2f29758743de5443aac24c5
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-22 17:41:59 -0600

    Create CalculateAge.java

---
+++ Other/Comp. Programming S2/ItsADate/CalculateAge.java
@@ -0,0 +1,58 @@
+// Nastassja Motro
+
+import java.util.Scanner;
+import java.util.Calendar;
+
+public class CalculateAge {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Year you were born in: ");
+    int year = keyboard.nextInt();
+    System.out.println("Month you were born in: ");
+    int month = keyboard.nextInt();
+    System.out.println("Day you were born: ");
+    Calendar now = Calendar.getInstance();
+    
+    public Date() {
+      this.millenniums = millenniums;
+      this.centuries = centuries;
+      this.decades = decades;
+      this.years = years;
+      this.months = months;
+      this.days = days;
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
+    }
+    
+    public int getMillennium() {
+      return this.millenniums;
+    }
+    public int getCenturies() {
+      return this.centuries;
+    }
+    public int getDecades() {
+      return this.decades;
+    }
+    public int getYears() {
+      return this.years;
+    }
+    public int getMonths() {
+      return this.months;
+    }
+    public int getDays() {
+      return this.days;
+    }
+    public int getHours() {
+      return this.hours;
+    }
+    public int getMinutes() {
+      return this.minutes;
+    }
+    public int getSeconds() {
+      return this.seconds;
+    }
+    }
+  }
+  System.out.println("You are " + milleniums + " millenia, " + centuries + " century, " + decades + " decade(s), " + years + " year(s), " + months + " month(s), " + days + " day(s), " + hours + " hour(s), " + minutes + " minute(s), " + seconds + " second(s) old.");
+}
