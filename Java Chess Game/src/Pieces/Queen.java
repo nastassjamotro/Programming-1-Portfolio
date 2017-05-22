400dc51772a00efca8c74d2745dfe72843f29d2a
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-22 17:18:26 -0600

    Update Queen.java

--- Java Chess Game/src/Pieces/Queen.java
+++ Java Chess Game/src/Pieces/Queen.java
@@ -1,1 +1,26 @@
+//Nastassja Motro
 
+public class Queen extends Piece{
+
+  public Queen(boolean available, int x, int y) {
+    super(available, x, y);
+  }
+  
+  @Override
+  public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
+    if(super.isValid(board, fromX, fromY, toX, toY) == false) {
+      return false;
+    }
+    // diagonal stuff
+    if(toX - fromX == toY - fromY) {
+      return true;
+    }
+    if(toX == fromX) {
+      return true;
+    }
+    if(toY == fromY) {
+      return true;
+    }   
+    return false;
+  }
+}
